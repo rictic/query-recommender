61cbaf5746cf1ca9cfdd61704470ad8812135a75
Peter Burns <peter@metaweb.com>
2010-05-16 00:36:25 -0700

    Initial pass at a "latest queries" server

---
+++ server.js
@@ -0,0 +1,63 @@
+var sys = require('sys');
+var http = require('http');
+var url = require('url');
+var net = require('net');
+
+var latest = [];
+// var most_popular = [];
+var results = {
+  latest: latest,
+//   most_popular: most_popular
+}
+http.createServer(function (request, response) {
+  var out = JSON.stringify(results);
+
+  var parts; 
+  try {
+    parts = url.parse(request.url, true)
+  }catch(e) {}
+  
+  if (parts && parts.query && parts.query.q)
+    log_query(parts.query.q);
+  
+  var callback;
+  if (parts && parts.query && parts.query.callback)
+    callback = parts.query.callback;
+  
+  response.writeHead(200, {'Content-Type': callback ? 'application/javascript' : 'application/json'});
+  if (callback)
+    out = callback + "(" + out + ")";
+  response.end(out);
+}).listen(8000, '0.0.0.0');
+
+
+var clients = [];
+net.createServer(function (stream) {
+  stream.setEncoding('utf8');
+  stream.addListener('connect', function () {
+    clients.push(stream);
+  });
+  stream.addListener('close', function () {
+    clients = removeElement(clients, stream);
+  });
+}).listen(7000, '0.0.0.0');
+
+
+function log_query(query) {
+  sys.puts(query);
+  latest.unshift(query);
+  while (latest.length > 7)
+    latest.pop();
+  
+  //broadcast:
+  clients.forEach(function(client) {
+    var was_sent = client.write(query + "\n");
+  })
+}
+
+function removeElement(array, value) {
+  var i = array.indexOf(value);
+  return array.slice(0,i).concat(l.slice(i+i,l.length))
+}
+
+sys.puts('Server running');
