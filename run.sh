b0af218d32e0386504e98d3975f627fa56822b88
Will Moffat <will_git@hamstersoup.com>
2010-05-22 13:44:07 +0000

    pass debug args to sever from run script

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #! /bin/sh
+ARGS=$*
 rm -f error.log
 ps aux | grep node | grep -v grep
 if [ $? -eq -0 ]; then
   echo 'Node already running!'
   exit 1
 fi
-nohup node server.js >> error.log &
+nohup node server.js $ARGS >> error.log &
 tail -f error.log
 
