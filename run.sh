c45c0d6d41bb00261b9134860273774927d9e135
Will Moffat <will_git@hamstersoup.com>
2010-05-22 15:35:08 +0000

    run script checks for node and kills it if possible

--- run.sh
+++ run.sh
@@ -1,11 +1,24 @@
 #! /bin/sh
 ARGS=$*
-rm -f error.log
-ps aux | grep node | grep -v grep
-if [ $? -eq -0 ]; then
-  echo 'Node already running!'
-  exit 1
+PID=`ps -eo pid,args | grep node | grep -v grep | cut -c1-6`
+if [ -n "$PID" ]; then
+  echo
+  echo 'Node already running! Press return to kill'
+  ps -e | grep node | grep -v grep
+  read dummy
+  /bin/kill $PID
+  if [ "$?" -ne "0" ]; then
+    echo 'Failed to kill node. Stopping'
+    exit 1
+  fi
+  echo "Waiting for node to die"
+  sleep 2
 fi
-nohup node server.js $ARGS >> error.log &
-tail -f error.log
+echo
+echo "Starting node"
+rm -f error.log
+nohup node server.js $ARGS > error.log &
+echo 
+echo "try running"
+echo "    tail -F error.log"
 
