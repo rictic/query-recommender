47763299cecc36441ae27fd7f2257b12914bf041
Peter Burns <peter@metaweb.com>
2010-05-25 16:31:39 -0700

    updated live stats to shorter key names

--- stats_live/stats.js
+++ stats_live/stats.js
@@ -9,8 +9,11 @@ var buffer = "";
 function handleData(data) {
   buffer += data;
   var match;
-  while(match = buffer.match(/(.*?)\n/)) {
-    handleRecord(JSON.parse(match[1]))
+  while(match = buffer.match(/^(.*?)\n/)) {
+    var data = match[1];
+    sys.puts(sys.inspect(data))
+    if (data.trim() !== "")
+      handleRecord(JSON.parse(data))
     buffer = buffer.substring(match.index+match[0].length);
   }
 }
@@ -20,9 +23,9 @@ var records = [];
 var TIMEFRAME = 60; //in seconds
 function handleRecord(record) {
   sys.print("\r                         \r")
-  sys.puts(record.query)
-  records.unshift(record.timestamp);
-  var recently = record.timestamp - TIMEFRAME * 1000;
+  sys.puts(record.q)
+  records.unshift(record.t);
+  var recently = record.t - TIMEFRAME * 1000;
   for (var i = records.length -1; ; i--) {
     if (records[i] >= recently)
       break
