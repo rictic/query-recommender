ba544b9380ef06800e3ea4e01feb28d97cc984a4
Peter Burns <peter@metaweb.com>
2010-05-25 22:21:55 -0700

    added reliability code to stats.js

--- stats_live/stats.js
+++ stats_live/stats.js
@@ -1,9 +1,31 @@
 var net = require('net')
 var sys = require('sys')
 
-var client = net.createConnection(7000, 'popular.youropenbook.org')
-client.addListener('data', handleData)
-
+var client;
+function createClient() {
+  sys.puts('connecting...')
+  client = net.createConnection(7000, process.argv[2] || 'popular.youropenbook.org')
+  var connected = false;
+  client.addListener("connect", function() {
+    connected = true;
+    sys.puts("connected");
+  })
+  client.addListener("error", restart)
+  client.addListener('data', handleData)
+  client.addListener("close", restart)
+  client.addListener("end", restart)
+  var restarted = false;
+  function restart() {
+    if (!restarted) {
+      if (connected)
+        sys.puts("connection lost")
+      setTimeout(createClient, 3000);
+    }
+      
+    restarted = true;
+  }
+}
+createClient()
 
 var buffer = "";
 function handleData(data) {
@@ -11,7 +33,6 @@ function handleData(data) {
   var match;
   while(match = buffer.match(/^(.*?)\n/)) {
     var data = match[1];
-    sys.puts(sys.inspect(data))
     if (data.trim() !== "")
       handleRecord(JSON.parse(data))
     buffer = buffer.substring(match.index+match[0].length);
@@ -22,8 +43,10 @@ function handleData(data) {
 var records = [];
 var TIMEFRAME = 60; //in seconds
 function handleRecord(record) {
+  var q = record.q || record.kind.q;
+  if (!q) return;
   sys.print("\r                         \r")
-  sys.puts(record.q)
+  sys.puts(q);
   records.unshift(record.t);
   var recently = record.t - TIMEFRAME * 1000;
   for (var i = records.length -1; ; i--) {
